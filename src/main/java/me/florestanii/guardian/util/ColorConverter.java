c0bac6f0e99150f905e7ad21bb9306c16002fcc5
leMaik <an@maik.tk>
2015-09-27 19:06:28 +0200

    Add converter for dye colors.

--- src/main/java/me/florestanii/guardian/util/ColorConverter.java
+++ src/main/java/me/florestanii/guardian/util/ColorConverter.java
@@ -3,6 +3,7 @@ package me.florestanii.guardian.util;
 import com.google.common.collect.Maps;
 import org.bukkit.ChatColor;
 import org.bukkit.Color;
+import org.bukkit.DyeColor;
 
 import java.util.Map;
 
@@ -31,5 +32,31 @@ public class ColorConverter {
 
     public static Color convertToColor(ChatColor color) {
         return chatColorColor.get(color);
+    }
+
+    private static Map<ChatColor, DyeColor> dyeColorColor;
+
+    static {
+        dyeColorColor = Maps.newHashMap();
+        dyeColorColor.put(ChatColor.BLACK, DyeColor.BLACK);
+        dyeColorColor.put(ChatColor.DARK_BLUE, DyeColor.BLUE);
+        dyeColorColor.put(ChatColor.DARK_GREEN, DyeColor.GREEN);
+        dyeColorColor.put(ChatColor.DARK_AQUA, DyeColor.CYAN);
+        dyeColorColor.put(ChatColor.DARK_RED, DyeColor.RED);
+        dyeColorColor.put(ChatColor.DARK_PURPLE, DyeColor.PURPLE);
+        dyeColorColor.put(ChatColor.GOLD, DyeColor.ORANGE);
+        dyeColorColor.put(ChatColor.GRAY, DyeColor.GRAY);
+        dyeColorColor.put(ChatColor.DARK_GRAY, DyeColor.GRAY);
+        dyeColorColor.put(ChatColor.BLUE, DyeColor.BLUE);
+        dyeColorColor.put(ChatColor.GREEN, DyeColor.GREEN);
+        dyeColorColor.put(ChatColor.AQUA, DyeColor.CYAN);
+        dyeColorColor.put(ChatColor.RED, DyeColor.RED);
+        dyeColorColor.put(ChatColor.LIGHT_PURPLE, DyeColor.PURPLE);
+        dyeColorColor.put(ChatColor.YELLOW, DyeColor.YELLOW);
+        dyeColorColor.put(ChatColor.WHITE, DyeColor.WHITE);
+    }
+
+    public static DyeColor convertToDyeColor(ChatColor color) {
+        return dyeColorColor.get(color);
     }
 }
